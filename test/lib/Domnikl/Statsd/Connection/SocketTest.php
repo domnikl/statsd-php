94d1c74b20bb1f35ddd97280b355358741d04a52
D-Rock <derek.gallo@gmail.com>
2012-08-03 14:56:06 -0400

    Refactored Connection class to Socket implementation behind an interface, allowing for other implementations such as Mocks or Nulls

--- test/lib/Domnikl/Statsd/Connection/SocketTest.php
+++ test/lib/Domnikl/Statsd/Connection/SocketTest.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Domnikl\Test\Statsd;
+namespace Domnikl\Test\Statsd\Connection;
 
-require_once __DIR__ . '/../../../../lib/Domnikl/Statsd/Connection.php';
+require_once __DIR__ . '/../../../../../lib/Domnikl/Statsd/Connection/Socket.php';
 
-class ConnectionTest extends \PHPUnit_Framework_TestCase
+class SocketTest extends \PHPUnit_Framework_TestCase
 {
     public function testInit()
     {
-        $connection = new \Domnikl\Statsd\Connection('localhost', 8125);
+        $connection = new \Domnikl\Statsd\Connection\Socket('localhost', 8125);
         $this->assertEquals('localhost', $connection->getHost());
         $this->assertEquals(8125, $connection->getPort());
     }
 
     public function testInitDefaults()
     {
-        $connection = new \Domnikl\Statsd\Connection();
+        $connection = new \Domnikl\Statsd\Connection\Socket();
         $this->assertEquals('localhost', $connection->getHost());
         $this->assertEquals(8125, $connection->getPort());
         $this->assertFalse($connection->forceSampling());
