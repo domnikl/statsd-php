d1cb2f7eb72ba8aa789a57421f78f575f5caa955
Dominik Liebler <liebler.dominik@googlemail.com>
2012-08-08 17:05:06 +0200

    removed var_dump()

--- lib/Domnikl/Statsd/Connection/Socket.php
+++ lib/Domnikl/Statsd/Connection/Socket.php
@@ -63,7 +63,7 @@ class Socket
     public function send($message)
     {
         if (0 != strlen($message) && $this->_socket) {
-            var_dump(fwrite($this->_socket, $message));
+            fwrite($this->_socket, $message);
         }
     }
 
