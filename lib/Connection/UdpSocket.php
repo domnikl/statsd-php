e922b241248460a61b3b1eca517e2fadc5cb026f
Matthias Endler <matthias-endler@gmx.net>
2015-01-28 18:17:31 +0100

    Set default udp persistence
    It might make sense to set the default connection handling of the udp protocol to non-persistent.
    Apart from having a simpler interface, there might be issues when creating too many persistent connections.
    (see here: http://php.net/manual/en/function.pfsockopen.php)

--- lib/Connection/UdpSocket.php
+++ lib/Connection/UdpSocket.php
@@ -38,7 +38,7 @@ class UdpSocket extends InetSocket implements Connection
      * @param int|null $timeout
      * @param bool $persistent
      */
-    protected function connect($host, $port, $timeout, $persistent)
+    protected function connect($host, $port, $timeout, $persistent = False)
     {
         $errorNumber = null;
         $errorMessage = null;
